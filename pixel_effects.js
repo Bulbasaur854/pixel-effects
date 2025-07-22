a05f23bd11633767f036caf95a67e7059debed90
Bul8a54ur <tairshechter1118@gmail.com>
2025-07-22 19:10:04 +0300

    load images using base64

--- pixel_effects.js
+++ pixel_effects.js
@@ -1,6 +1,7 @@
 // This project is based on the guide of 'freeCodeCamp.org' YouTube channel
 // https://www.youtube.com/watch?v=UoTxOVEecbI&list=WL&index=1&t=2600s&ab_channel=freeCodeCamp.org
 import { Particle } from "./particle.js";
+import * as ImagesB64 from "./images_b64.js";
 
 const NUM_OF_PARTICLES = 5000;
 
@@ -11,7 +12,8 @@ const image = new Image();
 let particles = [];
 let canvas_grid = [];
 
-image.src = "./image.png";
+image.src = ImagesB64.controller;
+
 image.addEventListener("load", () => {
     canvas.width = image.width;
     canvas.height = image.height;
@@ -28,7 +30,8 @@ function image_to_gray_scale(scanned_image) {
     const scanned_data = scanned_image.data;
 
     for (let i = 0; i < scanned_data.length; i += 4) {
-        const total = scanned_data[i] + scanned_data[i + 1] + scanned_data[i + 2]; // total = red + green + blue
+        const total =
+            scanned_data[i] + scanned_data[i + 1] + scanned_data[i + 2]; // total = red + green + blue
         const average_color_value = total / 3;
         scanned_data[i] = average_color_value;
         scanned_data[i + 1] = average_color_value;
@@ -41,8 +44,6 @@ function image_to_gray_scale(scanned_image) {
 
 function image_particles_overlay(scanned_image) {
     create_particles();
-    animate_particles();
-
     // map the scanned image data to an array of cells,
     // each cell holds its brightness and can be accessed by
     // using its x and y position as indexes
@@ -50,14 +51,17 @@ function image_particles_overlay(scanned_image) {
         let row = [];
         for (let x = 0; x < canvas.width; x++) {
             const red = scanned_image.data[y * 4 * scanned_image.width + x * 4];
-            const green = scanned_image.data[y * 4 * scanned_image.width + x * 4 + 1];
-            const blue = scanned_image.data[y * 4 * scanned_image.width + x * 4 + 2];
+            const green =
+                scanned_image.data[y * 4 * scanned_image.width + x * 4 + 1];
+            const blue =
+                scanned_image.data[y * 4 * scanned_image.width + x * 4 + 2];
             const brightness = get_relative_brightness(red, green, blue);
             const cell = [brightness];
             row.push(cell);
         }
         canvas_grid.push(row);
     }
+    animate_particles();
 }
 
 function create_particles() {
@@ -73,12 +77,16 @@ function animate_particles() {
     ctx.globalAlpha = 0.2;
     for (let i = 0; i < particles.length; i++) {
         particles[i].update(canvas_grid);
-        ctx.globalAlpha = particles[i].speed * 0.25;
+        ctx.globalAlpha = particles[i].speed * 0.2;
         particles[i].draw();
     }
     requestAnimationFrame(animate_particles);
 }
 
 function get_relative_brightness(red, green, blue) {
-    return Math.sqrt(red * red * 0.299 + green * green * 0.587 + blue * blue * 0.114) / 100;
+    return (
+        Math.sqrt(
+            red * red * 0.299 + green * green * 0.587 + blue * blue * 0.114
+        ) / 100
+    );
 }
