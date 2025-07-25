113be68c710c1eab9ad021749a49de239887d4f5
Bul8a54ur <tairshechter1118@gmail.com>
2025-07-25 09:49:53 +0300

    basic functionality

--- js/input.js
+++ js/input.js
@@ -1,19 +1,21 @@
-const pixel_number_container = document.getElementById("pixel-number-slider");
-const pixel_speed_container = document.getElementById("pixel-speed-slider");
+const pixel_number_slider = document.getElementById("pixel-number-slider");
+const pixel_number_text = document.getElementById("pixel-number-text");
+const pixel_speed_slider = document.getElementById("pixel-speed-slider");
+const pixel_speed_text = document.getElementById("pixel-speed-text");
 
-pixel_number_container.children[1].innerHTML = pixel_number_container.children[0].value;
-pixel_speed_container.children[1].innerHTML = pixel_speed_container.children[0].value;
+pixel_number_text.innerHTML = pixel_number_slider.value;
+pixel_speed_text.innerHTML = pixel_speed_slider.value;
 
-pixel_number_container.children[0].oninput = () => {
-    pixel_number_container.children[1].innerHTML = pixel_number_container.children[0].value;
+pixel_number_slider.oninput = () => {
+    pixel_number_text.innerHTML = pixel_number_slider.value;
 }
-pixel_speed_container.children[0].oninput = () => {
-    pixel_speed_container.children[1].innerHTML = pixel_speed_container.children[0].value;
+pixel_speed_slider.oninput = () => {
+    pixel_speed_text.innerHTML = pixel_speed_slider.value;
 }
 
 export function get_controls_values() {
     return { 
-        pixel_num:  pixel_number_container.children[0].value,
-        pixel_speed: pixel_speed_container.children[0].value
+        pixel_num:  pixel_number_slider.value,
+        pixel_speed: pixel_speed_slider.value
     }
 }
