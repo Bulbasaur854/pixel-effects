7a73b17d24314d4160f29ebf877a3587f83d74ea
Bul8a54ur <tairshechter1118@gmail.com>
2025-07-25 08:35:41 +0300

    controls hooked

--- js/input.js
+++ js/input.js
@@ -1,17 +1,19 @@
 const pixel_number_container = document.getElementById("pixel-number-slider");
-const global_alpha_container = document.getElementById("global-alpha-slider");
 const pixel_speed_container = document.getElementById("pixel-speed-slider");
 
 pixel_number_container.children[1].innerHTML = pixel_number_container.children[0].value;
-global_alpha_container.children[1].innerHTML = global_alpha_container.children[0].value;
 pixel_speed_container.children[1].innerHTML = pixel_speed_container.children[0].value;
 
 pixel_number_container.children[0].oninput = () => {
     pixel_number_container.children[1].innerHTML = pixel_number_container.children[0].value;
 }
-global_alpha_container.children[0].oninput = () => {
-    global_alpha_container.children[1].innerHTML = global_alpha_container.children[0].value;
-}
 pixel_speed_container.children[0].oninput = () => {
     pixel_speed_container.children[1].innerHTML = pixel_speed_container.children[0].value;
+}
+
+export function get_controls_values() {
+    return { 
+        pixel_num:  pixel_number_container.children[0].value,
+        pixel_speed: pixel_speed_container.children[0].value
+    }
 }
--- js/pixel_effects.js
+++ js/pixel_effects.js
@@ -3,54 +3,55 @@
 //
 // PNG images are taken from - https://www.stickpng.com/
 import { Particle } from "./particle.js";
+import { get_controls_values } from "./input.js";
 import * as ImagesB64 from "./images_b64.js";
-
-const NUM_OF_PARTICLES = 7000;
-const GLOBAL_ALPHA_TWEAK = 0.2;
-const PIXEL_VELOCITY_MULT = 0.2;
 
 const canvas = document.getElementById("effects-canvas");
 const ctx = canvas.getContext("2d");
+
 const image = new Image();
+image.src = ImagesB64.sunflower;
+image.onload = () => {
+    canvas.width = image.width;
+    canvas.height = image.height;
+}
 
 let particles = [];
 let canvas_grid = [];
+let is_running = true;
+let animation_id;
 
-image.src = ImagesB64.sunflower;
+document.getElementById("stop-button").onclick = () => {
+    stop_animation();
+};
+document.getElementById("run-button").onclick = () => {
+    stop_animation();
+    start_animation();
+};
 
-image.addEventListener("load", () => {
-    canvas.width = image.width;
-    canvas.height = image.height;
-
+function start_animation() {
+    is_running = true;
+    const { pixel_num, pixel_speed } = get_controls_values();
+    
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
     const scanned_image = ctx.getImageData(0, 0, canvas.width, canvas.height);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // image_to_gray_scale(scanned_image);
+    create_particles(pixel_num, pixel_speed);
+
     image_particles_overlay(scanned_image);
-    // setTimeout(() => {
-    //     image_particles_overlay(scanned_image);
-    // }, 10000)
-});
 
-function image_to_gray_scale(scanned_image) {
-    const scanned_data = scanned_image.data;
+    animate_particles();
+}
 
-    for (let i = 0; i < scanned_data.length; i += 4) {
-        const total =
-            scanned_data[i] + scanned_data[i + 1] + scanned_data[i + 2]; // total = red + green + blue
-        const average_color_value = total / 3;
-        scanned_data[i] = average_color_value;
-        scanned_data[i + 1] = average_color_value;
-        scanned_data[i + 2] = average_color_value;
+function create_particles(pixel_num, pixel_speed) {
+    for (let i = 0; i < pixel_num; i++) {
+        particles.push(new Particle(canvas, ctx, pixel_speed));
     }
-
-    const gray_image = new ImageData(scanned_data, canvas.width, canvas.height);
-    ctx.putImageData(gray_image, 0, 0);
+    console.log(particles)
 }
 
 function image_particles_overlay(scanned_image) {
-    create_particles();
     // map the scanned image data to an array of cells,
     // each cell holds its brightness and can be accessed by
     // using its x and y position as indexes
@@ -69,25 +70,6 @@ function image_particles_overlay(scanned_image) {
         }
         canvas_grid.push(row);
     }
-    animate_particles();
-}
-
-function create_particles() {
-    for (let i = 0; i < NUM_OF_PARTICLES; i++) {
-        particles.push(new Particle(canvas, ctx, PIXEL_VELOCITY_MULT));
-    }
-}
-
-function animate_particles() {
-    ctx.globalAlpha = 0.05;
-    ctx.fillStyle = "rgb(0, 0, 0)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < particles.length; i++) {
-        particles[i].update(canvas_grid);
-        ctx.globalAlpha = particles[i].speed * GLOBAL_ALPHA_TWEAK;
-        particles[i].draw(canvas_grid);
-    }
-    requestAnimationFrame(animate_particles);
 }
 
 function get_relative_brightness(red, green, blue) {
@@ -97,3 +79,39 @@ function get_relative_brightness(red, green, blue) {
         ) / 100
     );
 }
+
+function animate_particles() {
+    if (!is_running) { return; }
+    ctx.globalAlpha = 0.05;
+    ctx.fillStyle = "rgb(0, 0, 0)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update(canvas_grid);
+        ctx.globalAlpha = particles[i].speed * 0.5;
+        particles[i].draw(canvas_grid);
+    }
+    animation_id = requestAnimationFrame(animate_particles);
+}
+
+function stop_animation() {
+    if (animation_id) { cancelAnimationFrame(animation_id); }
+    is_running = false;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particles = [];
+}
+
+// function image_to_gray_scale(scanned_image) {
+//     const scanned_data = scanned_image.data;
+
+//     for (let i = 0; i < scanned_data.length; i += 4) {
+//         const total =
+//             scanned_data[i] + scanned_data[i + 1] + scanned_data[i + 2]; // total = red + green + blue
+//         const average_color_value = total / 3;
+//         scanned_data[i] = average_color_value;
+//         scanned_data[i + 1] = average_color_value;
+//         scanned_data[i + 2] = average_color_value;
+//     }
+
+//     const gray_image = new ImageData(scanned_data, canvas.width, canvas.height);
+//     ctx.putImageData(gray_image, 0, 0);
+// }
