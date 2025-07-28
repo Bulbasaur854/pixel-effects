0a1e1ded8a4a9cfd42dbdd0978d45c7e43bf1abf
Bulbasaur854 <tairshechter1118@gmail.com>
2025-07-28 16:34:49 +0300

    fix first particle spawn

--- js/particle.js
+++ js/particle.js
@@ -4,7 +4,7 @@ export class Particle {
         this.ctx = ctx;
         this.image_bounds = image_bounds
         this.x = this.get_random_x_position();
-        this.y = this.get_random_y_position();
+        this.y = 0;
         this.abs_x = Math.floor(this.x);
         this.abs_y = Math.floor(this.y);
         this.speed = 0;
